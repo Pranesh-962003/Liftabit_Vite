aee84c72ecfea95eb2d8f06db031780c4407e27f
Pranesh-962003 <pranesh962003.p@gmail.com>
2026-06-16 19:29:37 +0530

    Update ContactSection.jsx

--- src/pages/sections/ContactSection.jsx
+++ src/pages/sections/ContactSection.jsx
@@ -89,7 +89,7 @@ export default function ContactSection() {
                 <div className="c-icon">
                   <svg viewBox="0 0 24 24"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" /><polyline points="22,6 12,13 2,6" /></svg>
                 </div>
-                <div className="c-txt"><span>Email</span><strong>hello@liftabit.com</strong></div>
+                <div className="c-txt"><span>Email</span><strong>hr@liftabit.in</strong></div>
               </div>
               <div className="c-detail">
                 <div className="c-icon">
